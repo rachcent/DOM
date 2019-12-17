e3a2d97832bb6d1b485ab37e872d52832727a460
rachcent <35813484+rachcent@users.noreply.github.com>
2019-12-16 21:02:29 -0800

    complete

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
+let backgroundImage;
+let bgBtn;
+let backgroundButn;
+
+
+
+function preload(){
+  bg = loadImage('assets/ok.jpg');
+
+}
+
 function setup() {
-  createCanvas(400, 400);
+  noCanvas();
+  backgroundButn = select('#backgroundButton');
+  backgroundImage = select ('#backg');
+backgroundButn.style("background-color","#98FB98");
+
+
+
+  backgroundButn.mousePressed(toggleBg);
+
+
 }
 
 function draw() {
   background(220);
+}
+
+function toggleBg(){
+  backgroundImage.show();
 }
