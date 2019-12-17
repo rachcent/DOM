3e31c973c485f86e83d0a8c6854ca58439745d69
rachcent <35813484+rachcent@users.noreply.github.com>
2019-12-16 16:15:09 -0800

    change bg images

--- sketch.js
+++ sketch.js
@@ -1,5 +1,14 @@
+let imageBgs = [];
+
+function preload(){
+  for (let i = 0; i <=3;i++){
+    imageBgs[i] = loadImage(`assets/bg"_${i}.JPG`)
+  }
+}
+
 function setup() {
   createCanvas(400, 400);
+  console.log(imageBgs);
 }
 
 function draw() {
